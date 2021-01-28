504660064d4df555b04e0e6f757de3509e59994c
semarslan <sema.arslan@bil.omu.edu.tr>
2021-01-28 15:53:01 +0300

    Project validation added.

--- src/main/java/com/semarslan/projectmanagement/api/ProjectController.java
+++ src/main/java/com/semarslan/projectmanagement/api/ProjectController.java
@@ -5,10 +5,17 @@ import com.semarslan.projectmanagement.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,7 +25,16 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Project> save(@RequestBody Project project){
+    public ResponseEntity<?> save(@Valid @RequestBody Project project, BindingResult result) {
+
+        Map<String, String> errorMap = new HashMap<>();
+        if (result.hasErrors()) {
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.CREATED);
+        }
 
         projectService.saveOrUpdateProject(project);
 
