fbbe30dd32c9f76cd8358ac43ace9b32de0c0883
semarslan <sema.arslan@bil.omu.edu.tr>
2021-02-09 11:40:03 +0300

    create jwt token

--- src/main/java/com/semarslan/projectmanagement/api/UserController.java
+++ src/main/java/com/semarslan/projectmanagement/api/UserController.java
@@ -1,16 +1,25 @@
 package com.semarslan.projectmanagement.api;
 
+import com.semarslan.projectmanagement.auth.JwtTokenProvider;
 import com.semarslan.projectmanagement.entities.User;
+import com.semarslan.projectmanagement.payload.JWTLoginSuccessResponse;
+import com.semarslan.projectmanagement.payload.LoginRequest;
 import com.semarslan.projectmanagement.services.MapValidationErrorService;
 import com.semarslan.projectmanagement.services.UserService;
 import com.semarslan.projectmanagement.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static com.semarslan.projectmanagement.auth.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,9 +34,35 @@ public class UserController {
 
     private final UserValidator userValidator;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MApValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
 
+        /**
+         * validate passwords match
+         */
         userValidator.validate(user, result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MApValidationService(result);
---
+++ src/main/java/com/semarslan/projectmanagement/auth/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.semarslan.projectmanagement.auth;
+
+import com.semarslan.projectmanagement.entities.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.semarslan.projectmanagement.auth.SecurityConstants.EXPIRATION_TIME;
+import static com.semarslan.projectmanagement.auth.SecurityConstants.SECRET;
+
+@Component
+public class JwtTokenProvider {
+
+    /**
+     * Generate the token
+     */
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime()+EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+
+    /**
+     * VAlidate the token
+     */
+
+    /**
+     * Get user Id from token
+     */
+}
